ea67c6722421c3582e739a82ed7bb9019aecde32
Henrique Kioshi Yamauchi <101335626+hkioshi@users.noreply.github.com>
2022-05-23 22:08:15 -0300

    to conseguindo

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 function jogar()
 {
-    // Essa função é para inicar o quiz, ele vai fazer as tags de titulo
-    // e o botao jogar desaparecerem e a primeira questao aparecer
+  // Essa função é para inicar o quiz, ele vai fazer as tags de titulo
+  // e o botao jogar desaparecerem e a primeira questao aparecer
 
-    // Fazer tudo desaparecer
-    const
+  // Fazer tudo desaparecer
+  const desaparecerTitulo = document.getElementById("telaInicial");
+  desaparecerTitulo.style.display = "none";
+  questaoUm();
 
-    
-    
-    
-    
-      
-      
-    
+}
+
+function aparece()
+{
+  const desaparecerTitulo = document.getElementById("telaInicial");
+    desaparecerTitulo.style.display = "block";
+}
+
+function questaoUm
+{
+  
 }

9ba0e9a639c8d3d0688703789ff5f63cd62f425e
Henrique Kioshi Yamauchi <101335626+hkioshi@users.noreply.github.com>
2022-05-28 18:33:23 -0300

    questao 1 completa

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let cont;
+
+
 function jogar()
 {
   // Essa função é para inicar o quiz, ele vai fazer as tags de titulo
@@ -18,12 +21,40 @@ function aparece()
 
 function questaoUm()
 {
+  cont++;
   let questao = document.getElementById("questaoLayoutUm");
   let questaoNumero = document.getElementById("numeroId");
-  let questaoTitulo = document.getElementById("tituloId")
+  let questaoTitulo = document.getElementById("tituloId");
+  let alternativaUm = document.getElementById("quadrado1");
+  let alternativaDois = document.getElementById("quadrado2");
+  let alternativaTres = document.getElementById("quadrado3");
+  let alternativaQuatro = document.getElementById("quadrado4");
+  
+  alternativaUm.innerText = '2';
+  alternativaDois.innerText = '4';
+  alternativaTres.innerText = '6';
+  alternativaQuatro.innerText = '3';
+
   questaoNumero.innerText = '1';
   questaoTitulo.innerText = 'Quantos palitos tem aqui?';
   //questaoNumero.onclick(questaoDois());
-
+  
   questao.style.display = "block";
 }
+
+function perdeu()
+{
+  switch(cont)
+  {
+    case 1:
+      let questao = document.getElementById("questaoLayoutUm");
+    break;
+      
+  }
+
+  questao.style.display = "none";
+
+ 
+}
+
+
